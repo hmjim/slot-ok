455bc57d7da62728a334659ceb4b5cb1d241e5f9
Max <hmjim@mail.ru>
2021-10-19 19:49:14 +0700

    19.10

--- index.php
+++ index.php
@@ -296,7 +296,9 @@ curl_close( $ch );
 $bodytag = str_replace( "((?!slot-ok3\.info/b)\w+(?:\.\w+)+", "https://slot-ok.azurewebsites.net/", $contents );
 $result  = preg_replace( '~' . $new_url . '~m', "https://slot-ok.azurewebsites.net", $contents );
 preg_match_all( "/(https:\/\/slot-ok3.info).*\.(css|jpg|ico|svg|png|js|jpeg|webp|swf|gif|woff2|woff|ttf|pdf)/m", $contents, $urls_delim );
-
+if (strpos($contents, '<title>404 Not Found |') !== false) {
+	header("HTTP/1.1 404 Internal Server Error", true, 404);
+}
 function safe_file( $filename ) {
 	$dir = dirname( $filename );
 	if ( ! file_exists( __DIR__ . $dir ) ) {
